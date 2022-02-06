02ee705fcc832b429127c7dd109f44cd312e98c9
AleTech04 <99140003+AleTech04@users.noreply.github.com>
2022-02-06 21:57:45 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
-sap.ui.define([
-	"sap/m/Button"
-], function (Button, XMLView) {
+/*sap.ui.define([
+	"sap/ui/core/mvc/XMLView"
+], function (XMLView) {
 	"use strict";
 
-	new Button({
-		text: "Hello World"
-	}).placeAt("content")
+	XMLView.create({
+		viewName: "sap.ui.demo.view.App"
+	}).then(function (oView) {
+		oView.placeAt("content");
+	});
 
+});*/
+
+sap.ui.define([
+	"sap/ui/core/ComponentContainer"
+], function (ComponentContainer) {
+	"use strict";
+
+	new ComponentContainer({
+		name: "sap.ui.demo",
+		settings : {
+			id : "app"
+		},
+		async: true
+	}).placeAt("content");
 });
